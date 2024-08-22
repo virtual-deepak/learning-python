ea1a526da3ed9d482825e28fd2b81e0d11d39cbc
Deepak Agarwal <deep.agarwal@hotmail.com>
2024-08-22 23:39:38 +0530

    Added skip as mark and ids in pytest

--- phonebook/with_pytest/test_phonebook.py
+++ phonebook/with_pytest/test_phonebook.py
@@ -33,10 +33,15 @@ def test_empty_phonebook_is_consistent(phonebook):
         (("Bob", "12345"), ("Sue", "67890"), True),
         (("Bob", "12345"), ("Sue", "12345"), False),
         (("Bob", "12345"), ("Sue", "123"), False),
-    ],
+    ], 
+    ids=["Distinct Values", "Same Values", "Same Prefix"]
 )
 def test_is_consistent(phonebook, entry1, entry2, expected_is_consistent):
     phonebook.add(*entry1)
     phonebook.add(*entry2)
     actual_is_consistent = phonebook.is_consistent()
     assert actual_is_consistent == expected_is_consistent
+
+@pytest.mark.skip("Work in progress")
+def test_phonebook_new_feature():
+    assert False
